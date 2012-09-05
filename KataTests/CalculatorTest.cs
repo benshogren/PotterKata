b939861f407349430236bc9257df68b3d7d4e6ab
unknown <benshogren@gmail.com>
2012-09-05 11:46:04 -0400

    Initial commit of PotterKata

---
+++ KataTests/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PotterKata;
+
+namespace KataTests {
+
+    [TestFixture]
+    public class CalculatorTest {
+
+        public BookPriceCalculator b = new BookPriceCalculator();
+
+        [Test, TestCaseSource("Source")]
+        public void Test1(List<int> list, decimal price) {
+            var bookList = b.Price(list);
+            Assert.AreEqual(price, bookList);
+        }
+      
+        static object[] Source = 
+        {
+            new object[] {new List<int> {1}, 8m},
+            new object[] {new List<int> { }, 0m},
+            new object[] {new List<int> {1, 1}, 15.2m},
+            new object[] {new List<int> {1, 1, 2}, 29.6m},
+            new object[] {new List<int> {1, 1, 2, 2, 1}, 46m},
+            new object[] {new List<int> {1, 1, 4, 3}, 65.6m}
+        };
+       
+    }
+}
---
+++ PotterKata/BookPriceCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterKata {
+    public class BookPriceCalculator {
+        
+        public decimal Price(List<int> bookList) {
+            int totalNumberOfBooks = bookList.Sum();
+            int numberInSet = bookList.Count;
+            int nonDiscountedBooks = totalNumberOfBooks - numberInSet;
+            decimal costOfNonDiscountedBooks = nonDiscountedBooks * 8;
+            decimal costOfBooksInSet = (bookList.Count * 8) * (Discounts[bookList.Count]);
+            decimal finalCost = costOfNonDiscountedBooks + costOfBooksInSet;
+            return finalCost;
+        }
+
+        private Dictionary<int, decimal> Discounts = new Dictionary<int, decimal> {
+            {0, 0m},   // zero books gets zero discount
+            {1, 1m},   // no discount for one book
+            {2, .95m}, // 5 percent discount for two books
+            {3, .90m}, // 10 percent discount for three books
+            {4, .80m}, // 20 percent discount for four books
+            {5, .75m}  // 25 percent discount for five books
+        };
+
+
+
+
+
+
+
+
+
+
+    }
+}
