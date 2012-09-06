fec48b6513cfcf53c2b568d31aa1d3b648a5cd75
Ben Shogren <benshogren@gmail.com>
2012-09-06 10:17:44 -0700

    Continuing on new machine

---
+++ KataTests/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PotterKata;
+
+namespace KataTests {
+
+    [TestFixture]
+    public class CalculatorTest {
+
+        public BookPriceCalculator b = new BookPriceCalculator();
+
+        [Test, TestCaseSource("Source")]
+        public void Test1(List<int> list, decimal price) {
+            var bookList = b.Price(list);
+            Assert.AreEqual(price, bookList);
+        }
+      
+        static object[] Source = 
+        {
+            new object[] {new List<int> {1}, 8m},
+            new object[] {new List<int> { }, 0m},
+            new object[] {new List<int> {1, 1}, 15.2m},
+            new object[] {new List<int> {1, 1, 2}, 29.6m},
+
+            new object[] {new List<int> {2, 2, 1, 1, 1}, 0m},
+            new object[] {new List<int> {2, 2, 2, 1, 1}, 51.2m},
+            new object[] {new List<int> {2, 2, 2, 2, 1}, 0m}
+        };
+       
+    }
+}
---
+++ PotterKata/BookPriceCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterKata {
+    public class BookPriceCalculator {
+        
+        public decimal Price(List<int> bookList) {
+            int totalNumberOfBooks = bookList.Sum();
+            int numberInSet = bookList.Count;
+            int remainderBooks = totalNumberOfBooks - numberInSet;
+            if (remainderBooks > 1) {
+                decimal costOfSecondSet = (remainderBooks * 8) * Discounter(remainderBooks, bookList);
+            }
+            
+            
+            decimal costOfNonDiscountedBooks = remainderBooks * 8;
+            decimal costOfBooksInSet = (bookList.Count * 8) * (Discounts[bookList.Count]);
+            
+            
+            
+
+
+
+            return 0;
+        }
+
+        public decimal Discounter(int set, List<int> bookList) {
+            return Discounts[bookList.Count];
+        }
+
+        private Dictionary<int, decimal> Discounts = new Dictionary<int, decimal> {
+            {0, 0m},   
+            {1, 1m},   // no discount for one book
+            {2, .95m}, // 5 percent discount for two books
+            {3, .90m}, // 10 percent discount for three books
+            {4, .80m}, // 20 percent discount for four books
+            {5, .75m}  // 25 percent discount for five books
+        };
+    }
+}
