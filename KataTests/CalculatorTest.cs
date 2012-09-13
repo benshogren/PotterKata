031e38cb51bb468f8fd22bfa6433bc74740fc67f
Ben Shogren <benshogren@gmail.com>
2012-09-13 09:32:47 -0700

    Working on all possible combinations algorithm to determine best possible discount.

--- KataTests/CalculatorTest.cs
+++ KataTests/CalculatorTest.cs
@@ -9,9 +9,9 @@ namespace KataTests {
 
     [TestFixture]
     public class CalculatorTest {
-
+        
         public BookPriceCalculator b = new BookPriceCalculator();
-
+        
         [Test, TestCaseSource("Source")]
         public void Test1(List<int> list, decimal price) {
             var bookList = b.Price(list);
@@ -28,6 +28,29 @@ namespace KataTests {
             new object[] {new List<int> {2, 2, 1, 1}, 40.8m},
             new object[] {new List<int> {2, 2, 2, 1, 1}, 51.2m}
         };
-       
+
+        [Test]
+        public void TestPerm()
+        {
+            Assert.AreEqual(3, b.Perm(3, 2));
+        }
+        [Test]
+        public void TestCombos()
+        {
+            Assert.AreEqual(
+                new List<List<int>>{
+                    new List<int>{1},
+                    new List<int>{2},
+                    new List<int>{3},
+                    new List<int>{1, 2},
+                    new List<int>{1, 3},
+                    new List<int>{2, 3},
+                    new List<int>{1, 2, 3}
+                },
+                b.AllPossibleCombinations(new List<int>{1, 2, 3}, 3));
+        }
+
+
+
     }
 }
--- PotterKata/BookPriceCalculator.cs
+++ PotterKata/BookPriceCalculator.cs
@@ -18,16 +18,62 @@ namespace PotterKata {
             if (remainderBooks > 1) {
                 decimal SecondarySet = PriceChecker(remainderBooks);
                 finalPrice = PrimarySet + SecondarySet;
-            } else finalPrice = PrimarySet + NonSetBooks;
-            
-
-
-            
-
-
-
+            } else finalPrice = PrimarySet + NonSetBooks; 
             return finalPrice;
         }
+
+
+
+        public int Perm(int a, int b = 0, int c = 0, int d = 0, int e = 0){
+            List<int> set = new List<int> {a};
+            if (b > 0) set.Add(b);
+            if (c > 0) set.Add(c);
+            if (d > 0) set.Add(d);
+            if (e > 0) set.Add(e);
+
+
+
+            return set[0];
+        }
+
+        public List<List<int>> AllPossibleCombinations(List<int> books, int size) {
+
+           
+            List<List<int>> combination = new List<List<int>>();
+
+            //var enumerator = books.GetEnumerator();
+            
+            //int actual = enumerator.Current;
+            //enumerator.MoveNext();//maybe switch
+
+
+            //List<int> subSet = new List<int>(books);
+            //subSet.Remove(actual);
+
+            //List<List<int>> subSetCombination = AllPossibleCombinations(subSet, size - 1);
+
+            //foreach (List<int> set in subSetCombination) {
+            //    List<int> newSet = new List<int>(set);
+            //    List<int> ActualZero = new List<int> { 0, actual };
+            //    newSet.AddRange(ActualZero);
+            //    combination.Add(newSet);
+            //}
+
+            //combination.AddRange(AllPossibleCombinations(subSet, size));
+
+
+            return combination;
+        }
+
+
+
+
+
+
+
+
+
+
 
         public decimal PriceChecker(int numberOfBooks) { 
             return (numberOfBooks * 8) * Discounts[numberOfBooks];
@@ -42,4 +88,6 @@ namespace PotterKata {
             {5, .75m}  // 25 percent discount for five books
         };
     }
+
+    
 }
